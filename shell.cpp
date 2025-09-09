9463c64831900ef6c8a83dfd6e5e19d0b94429e4
TShaw0 <shaw.trenton06@gmail.com>
2025-09-09 22:30:32 +0000

    lab 2

--- shell.cpp
+++ shell.cpp
@@ -15,31 +15,21 @@ int main () {
 
     // TODO: add functionality
     // Create pipe
-    pid_t pid = fork();
-    if (pid == 0) {
-      char* args[] = {const_cast<char*>("ls"), const_cast<char*>("-1"), NULL};
-      execvp("ls", args);
-      perror("execvp failed");
-      exit(1);
-    }
-    else if (pid > 0) {
-      int status;
-      waitpid(pid, &status, 0);
-      std::cout << "Child process finished with status: " << status << std::endl;
-    }
-    else{
-      perror("fork failed");
-      return 1;
-    }
-    // Create child to run first command
-    // In child, redirect output to write end of pipe
-    // Close the read end of the pipe on the child side.
-    // In child, execute the command
     int fd[2];
     if (pipe(fd) == -1){
       perror("pipe failed");
       return 1;
     }
+    int saved_stdin = dup(STDIN_FILENO);
+    int saved_stdout = dup(STDOUT_FILENO);
+    if (saved_stdin == -1 || saved_stdout == -1) {
+      perror("dup failed");
+      exit(1);
+    }
+    // Create child to run first command
+    // In child, redirect output to write end of pipe
+    // Close the read end of the pipe on the child side.
+    // In child, execute the command
     pid_t child1 = fork();
     if (child1 == 0) {
       dup2(fd[1], STDOUT_FILENO);
@@ -49,15 +39,6 @@ int main () {
       execvp("ls", args);
       perror("execvp failed (ls)");
       exit(1);
-    }
-    else if (child1 > 0) {
-      int status1;
-      waitpid(child1, &status1, 0);
-      std::cout << "Child process finished with status: " << status1 << std::endl;
-    }
-    else{
-      perror("fork failed");
-      return 1;
     }
     // Create another child to run second command
     // In child, redirect input to the read end of the pipe
@@ -73,20 +54,13 @@ int main () {
       perror("execvp failed (tr)");
       exit(1);
     }
-    else if (child2 > 0) {
-      int status2;
-      waitpid(child2, &status2, 0);
-      std::cout << "Child process finished with status: " << status2 << std::endl;
-    }
-    else{
-      perror("fork failed");
-      return 1;
-    }
     // Reset the input and output file descriptors of the parent.
     close(fd[0]);
     close(fd[1]);
-    int status3;
-    waitpid(child1, &status3, 0);
-    waitpid(child2, &status3, 0);
+    int status;
+    waitpid(child1,&status,0);
+    waitpid(child2,&status,0);
+    close(saved_stdin);
+    close(saved_stdout);
     return 0;
 }
